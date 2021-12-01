8571bd736a543b371eb5c027615696c43a1b3b3a
majun2232 <mj@cnbisoft.com>
2021-12-01 23:40:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-tgbjx4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-tgbjx4)
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+// pages/index.vue
+<template>
+  <div>
+    <h1>Index Page</h1>
+  </div>
+</template>
+
+// app.vue
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <NuxtPage />
+  </div>
+</template>
