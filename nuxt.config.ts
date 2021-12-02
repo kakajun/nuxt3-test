971bd46e70557db521d711e53f078cd98fada4a5
majun2232 <mj@cnbisoft.com>
2021-12-03 00:08:48 +0800

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   meta: {
     title: 'Vitesse Nuxt 3',
   },
-  buildModules: [
-
-    '@pinia/nuxt',
-  ],
-})
+  buildModules: [],
+});
