4f3acc73de87b09aa100afc2ad3f743d292d1dc7
majun2232 <mj@cnbisoft.com>
2021-12-02 23:15:58 +0800

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Index Page</h1>
+    <div>
+      {{ foo }}
+    </div>
     <NuxtLink to="/detail">Detail Page</NuxtLink>
   </div>
 </template>
+<script setup>
+const foo = useFoo();
+</script>
