4d84e7c05017ee9185e6b056ce5bf7f8ce0ba7ab
majun2232 <>
2021-12-03 02:57:52 +0000

    Deploying to gh-pages from @ majun2232/nuxt3-test@2fef985b881a1f31b221eb5bf1796aefb00db997 🚀

---
+++ _nuxt/TheFooter-790d12c0.mjs
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t,a as c}from"./entry-100d67cc.mjs";const n={},_=c("h1",null,"footer",-1),r=[_];function s(a,d){return t(),o("div",null,r)}var f=e(n,[["render",s]]);export{f as default};
---
+++ _nuxt/TheHeader-2c75322c.mjs
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t,a as n}from"./entry-100d67cc.mjs";const o={},_=n("h1",null,"heard",-1),r=[_];function s(a,d){return t(),c("div",null,r)}var f=e(o,[["render",s]]);export{f as default};
