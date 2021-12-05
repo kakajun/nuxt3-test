ef09eea16ab18b1ee9df6f935ee17bf5be016a23
卡卡军 <38216933+majun2232@users.noreply.github.com>
2021-12-05 11:12:03 +0800

    fix: 1

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,21 @@
     <div>
       {{ foo }}
     </div>
+    <div>
+      {{ $hello() }}
+    </div>
     <NuxtLink to="/detail">Detail Page</NuxtLink>
+    <div>
+      <NuxtLink to="/vueUse">vueUse Page</NuxtLink>
+    </div>
+    <div>
+      <NuxtLink to="/uitest">UI组件库测试</NuxtLink>
+    </div>
     <InputEntry />
     <!-- <NuxtLink to="/vueUse">vueUse Page</NuxtLink> -->
   </div>
 </template>
 <script setup>
+const { $hello } = useNuxtApp();
 const foo = useFoo();
 </script>
---
+++ pages/uitest.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <d-button>按钮</d-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
