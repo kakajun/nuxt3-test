cd8b529caebfa9ffef6b5136876988bdf1dc27d5
卡卡军 <38216933+majun2232@users.noreply.github.com>
2021-12-05 20:36:46 +0800

    fix: 配置打包路径

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,16 @@ export default defineNuxtConfig({
   meta: {
     title: 'Nuxt 3',
   },
+  buildDir:'',
   buildModules: [
     '@vueuse/core/nuxt',
     '@pinia/nuxt',
   ],
-
+ app: {
+    "basePath": "/nuxt3-test/",
+    "assetsPath": "/nuxt3-test/_nuxt/",
+    "cdnURL": null
+  },
   vite: {
     logLevel: 'info',
   },
