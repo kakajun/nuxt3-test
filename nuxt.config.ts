940cc5e0bfe9a244bf82173ac11d3677e3b7dd6e
卡卡军 <38216933+majun2232@users.noreply.github.com>
2021-12-05 22:08:27 +0800

    fix: 改端口

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,11 +11,11 @@ export default defineNuxtConfig({
     '@vueuse/core/nuxt',
     '@pinia/nuxt',
   ],
-//  app: {
-//     "basePath": "/fff/",
-//     "assetsPath": "./_nuxt/",
-//     "cdnURL": null
-//   },
+ "app": {
+    "basePath": "./",
+    "assetsPath": "./_nuxt/",
+    "cdnURL": null
+  },
   vite: {
     logLevel: 'info',
   },
