dfd8f1e4fec51d5dad90f1838cfa66304876ada1
卡卡军 <38216933+majun2232@users.noreply.github.com>
2021-12-06 08:06:15 +0800

    fix: 加入cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,19 +5,22 @@ export default defineNuxtConfig({
     title: 'Nuxt 3',
   },
     // buildDir: 'nuxt-build',
-  // rootDir:"/project",
- router: {
-    base: process.env.NUXT_ROUTER_BASE || '/'
-  },
+  // rootDir:"./project/",
+//  router: {
+//     base: process.env.NUXT_ROUTER_BASE || '/'
+//   },
+  // nitro: {
+  //   preset: '/nuxt3-test/'
+  // },
   buildModules: [
     '@vueuse/core/nuxt',
     '@pinia/nuxt',
   ],
- "app": {
-    "basePath": "./",
-    "assetsPath": "./_nuxt/",
-    "cdnURL": null
-  },
+//  "app": {
+//     // "basePath":  process.env.NUXT_ROUTER_BASE || '/',
+//     // "assetsPath": process.env.NUXT_ROUTER_BASE || '/'+"./_nuxt/",
+//     "cdnURL": null
+//   },
   vite: {
     logLevel: 'info',
   },
