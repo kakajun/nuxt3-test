a17f04f8c8a5fdb4330d4a4dd8b46b81e56e0dd9
majun2232 <>
2021-12-07 00:39:10 +0000

    Deploying to gh-pages from @ majun2232/nuxt3-test@ae3d4fdb5aba8396b993c0d59399c6f6170eb442 🚀

---
+++ _nuxt/Button-403769ec.mjs
@@ -0,0 +1,1 @@
+import{_ as t,c as n,l as o,o as s}from"./entry-144dee83.mjs";const _={},r={class:"button"};function a(e,c){return s(),n("a",r,[o(e.$slots,"default")])}var u=t(_,[["render",a]]);export{u as default};
